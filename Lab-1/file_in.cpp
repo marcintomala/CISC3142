7aaf221214113a462eeba06f8526839a61e8ba9c
Marcin Tomala <marcin.p.tomala@gmail.com>
2022-02-14 00:04:45 -0500

    implemented feature 2; fixed Makefile to run the compiled binary from the appropriate directory; added comments

--- Lab-1/file_in.cpp
+++ Lab-1/file_in.cpp
@@ -1,5 +1,5 @@
 // This file shows basic functions of reading in a CSV file using C++98
-// Please add your name to your code!
+// Marcin Tomala, CISC. 3142, Brooklyn College
 
 /* Homework Objective: fix this program to match the requirements listed:
   1. successfully reads all lines of the input file
@@ -8,7 +8,6 @@
   4. for each unique year, list the count of skus and also print out the skus as a list i.e. 2000 (3): 111, 211, 311 and make a new line per year.
 
 All programs must be able to compile in C++98 standard (the default version on Linux Machine). That means you cannot use the new features of C++11! :(
-
 */
 
 #include <iostream>
@@ -21,11 +20,14 @@ All programs must be able to compile in C++98 standard (the default version on L
 
 using namespace std;
 
-vector< vector<string> > read_data() {
 
-  vector< vector<string> > data(5);
+// Core logic functions:
+vector<vector<string> > read_data() {
+  // Data reader; reads in each line from the .csv file and distributes it in a vector of vectors of strings.
 
-  // define variables
+  // Given that there are 5 columns, we initialize the vector of vectors to 5 and initilize their variables.
+  // To enable universal usage, the vector of vectors matrix/data frame could be made dynamic - by e.g. analyzing the header 
+  vector<vector<string> > data(5);
   string sku, brand, category, year, price;
 
   ifstream in_stream;
@@ -38,27 +40,28 @@ vector< vector<string> > read_data() {
 
     while (getline(in_stream, lines)) { //while the end of file is NOT reached
 
-     // Fields: sku,brand,category,year,price
-     stringstream line (lines);
+      // Instead of parsing the file directly as in the starter code, we parse each line separately:
+      stringstream line (lines);
 
-     getline(line, sku, ',');
-     data[0].push_back(sku);
+      // Each member of each column is placed in its vector 'as is', without parsing of numerical values;
+      // That step is deferred until their numericity is needed for mean computations.
+      getline(line, sku, ',');
+      data[0].push_back(sku);
 
-     getline(line, brand, ',');
-     data[1].push_back(brand);
+      getline(line, brand, ',');
+      data[1].push_back(brand);
 
-     getline(line, category, ',');
-     data[2].push_back(category);
+      getline(line, category, ',');
+      data[2].push_back(category);
 
-     getline(line, year, ',');
-     data[3].push_back(year);
+      getline(line, year, ',');
+      data[3].push_back(year);
 
-     getline(line, price, '\n');
-     data[4].push_back(price);
-
+      getline(line, price, '\n');
+      data[4].push_back(price);
     }
 
-    in_stream.close(); //closing the file cout << "Number of entries: " << i-1 << endl;
+    in_stream.close(); //closing the file
 
    } else {
     cout << "Unable to open file";
@@ -67,25 +70,30 @@ vector< vector<string> > read_data() {
   return data;
 }
 
-map<string, float> compute_average(vector< vector<string> > data, int col) {
-  map< string, int > count;
-  map< string, float > price_total;
-  map< string, float > price_average;
+map<string, float> compute_average(vector<vector<string> > data, int col) {
+  // Feature 1: Average computer; given the vector of vectors and the desired 'group-by' variable (column index col), 
+  // returns a mapping of unique keys to their price average, in ascending order of keys (standard, ordered map).
+
+  map<string, int> count; // to maintain the number of products of the given kind 
+  map<string, float> price_total; // to maintain the price total of the given kind 
+  map<string, float> price_average;
 
   for (int i = 0; i < data[col].size(); i++) {
 
-    float t;
-    t = atof(data[4][i].c_str());
+    // Here prices get parsed to float using atof (which is a C standard library function - hence the .c_str() cast to a C string)
+    float t = atof(data[4][i].c_str());
 
+    // If the variable is already present in the map - increment the count and add the price from the current row.
     if (count.count(data[col][i])) {
       count[data[col][i]]++;
       price_total[data[col][i]] += t;
-    } else {
+    } else { // Otherwise, initialize its count to 1 and the price total with the price from the current row.
       count[data[col][i]] = 1;
       price_total[data[col][i]] = t;
     }
   }
 
+  // We iterate over the map keys to produce an average for each key before returning the variable:average mapping in line 101. 
   for (map<string, int>::iterator iter = count.begin(); iter != count.end(); ++iter) {
     string k = iter -> first;
     price_average[k] = price_total[k] / count[k];
@@ -94,36 +102,83 @@ map<string, float> compute_average(vector< vector<string> > data, int col) {
   return price_average;
 }
 
+map<string, vector<string> > skus_by_year(vector<vector<string> > data){
+  // Feature 2: SKU lister. Accepts the vector of vectors and constructs a <year>:<vector of SKU mapping>.
+  // Neither the year nor the SKU need to be parsed as integers for this, although the current string comparison
+  // underlying the ordered map *might* be coming out to be slightly more expensive. 
 
+  map<string, vector<string> > by_year;
+  for (int i = 0; i < data[0].size(); i++) {
+
+    string year = data[3][i];
+    string sku = data[0][i];
+    
+    // For each row of the vector of vectors matrix, we check if the year's already in the map. If so - we push the current SKU.
+    if (by_year.count(year)) {
+      by_year[year].push_back(sku);
+    } else { // Otherwise, we initialize a new vector for the new year and push the current SKU into it before placing it in the map.
+      vector<string> vect(1);
+      vect.push_back(sku);
+      by_year[year] = vect;
+    }
+  }
+  return by_year;
+}
+
+// Output functions:
+void output_data(vector<vector<string> > data) {
+  // Simply outputs the entire vector of vectors.
+  cout << "SKU" << "\t" << "Brand" << "\t" << "Category" << "\t" << "Year" << "\t" << "Price\n";
+  for (int i = 0; i < data[0].size(); i++) {
+    cout << data[0][i] << "\t" << data[1][i] << "\t" << data[2][i] << "\t" << data[3][i] << "\t" << data[4][i] + "\n";
+  }
+}
+
+void output_averages(map<string, float> prices) {
+  // Iterates over the computed price averages and outputs them (since the format of the map is the same 
+  // regardles of which variable was chosen as the 'pivot', this function is category agnostic).
+
+  for (map<string, float>::iterator iter = prices.begin(); iter != prices.end(); ++iter) {
+    string k = iter -> first;
+    float v = iter -> second;
+    cout << k << "\t" << v << "\n";
+  }
+}
+
+void output_year_skus(map<string, vector<string> > by_year) {
+  // Iterates over the year to SKU list mapping and outputs them in the specified format "Year (SKU count): SKU1, SKU2, ... , SKUn"
+  cout << "\n" <<  "Year" << "\t" << "SKUs" << "\n"; 
+  for (map<string, vector<string> >::iterator iter = by_year.begin(); iter != by_year.end(); ++iter) {
+    string k = iter -> first;
+    vector<string> v = iter -> second;
+    string years_skus = "";
+    for (int i = 0; i < v.size(); i++) {
+      years_skus += v[i];
+      if (i > 0 && i != v.size()-1) { // no comma before first and after last SKU
+        years_skus += ", ";
+      }
+    }
+    cout << k << " (" << v.size()-1 << "): " << years_skus << "\n";
+  }
+}
 
 int main() {
-
+  ofstream out_stream;
+  out_stream.open("data.txt");
   //output values
-  cout << "SKU" << "\t" << "Brand" << "\t" << "Category" << "\t" << "Year" << "\t" << "Price\n";
-  vector< vector<string> > data = read_data();
-
-  for (int i = 0; i < data[0].size(); i++) {
-    cout << data[0][i] << "\t" << data[1][i] << "\t" << data[2][i] << "\t" << data[3][i] << "\t" << data[4][i] + "\n"; 
-  }
+  vector<vector<string> > data = read_data();
+  output_data(data);
 
   map<string, float> brand_prices = compute_average(data, 1); // column 1 = brand
-
   cout << "\n" <<  "Brand" << "\t" << "Average Price" << "\n"; 
-  for (map<string, float>::iterator iter = brand_prices.begin(); iter != brand_prices.end(); ++iter) {
-    string k = iter -> first;
-    float v = iter -> second;
-    cout << k << "\t" << v << "\n";
-  }
+  output_averages(brand_prices);
 
   map<string, float> category_prices = compute_average(data, 2); // column 2 = category
-
   cout << "\n" <<  "Category" << "\t" << "Average Price" << "\n"; 
-  for (map<string, float>::iterator iter = category_prices.begin(); iter != category_prices.end(); ++iter) {
-    string k = iter -> first;
-    float v = iter -> second;
-    cout << k << "\t" << v << "\n";
-  }
+  output_averages(category_prices);
 
-  cout << endl;
+  map<string, vector<string> > by_year = skus_by_year(data);
+  output_year_skus(by_year);
 
+  return 0;
 }
--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 lab1:
 	g++ ./Lab-1/file_in.cpp -o ./Lab-1/file_in.o -std=c++98
-	./Lab-1/file_in.o
+	cd Lab-1 && ./file_in.o
